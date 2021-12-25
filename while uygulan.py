adb65f90cda08f9a8c9ed316cbd12f0ccf2ab07c
nere9eren <96655774+nere9eren@users.noreply.github.com>
2021-12-25 16:51:44 +0300

    Add files via upload

---
+++ while uygulan.py
@@ -0,0 +1,9 @@
+while True:
+    kullanici=input("kullanici girin").lower()
+    sifre=input("sifre giriniz")
+    if kullanici=="admin" and sifre=="1234":
+        print("basarılı")
+        print("sisteme girildi")
+        break
+    else
+        print("kullanici veya sifre hatali")
